fe5c4eedb467f6375fbac6a996a798dfe9392eef
chaoswork612 <konstantin-morozov80@yandex.ru>
2019-06-09 14:34:02 +0300

    Added hamburgers.js

--- lesson_2/hamburgers/hamburgers.js
+++ lesson_2/hamburgers/hamburgers.js
@@ -1,47 +1,65 @@
 class Hamburger {
     constructor(size, stuffing, toppings = null) {
+        this.sizeList = this._renderSizeList();
+        this.stuffingList = this._renderStuffingList();
+        this.toppingList = this._renderToppingList();
         this.size = this.getSize(size);
         this.stuffing = this.getStuffing(stuffing);
         this.toppings = this.getToppings(toppings);
     }
 
-    getStuffing(stuffing) {
-        let result;
-        this.stuffing = [
+    _renderSizeList() {
+        this.sizeList = [
+            {name: 'Small', price: 50, calories: 20},
+            {name: 'Big', price: 100, calories: 40}
+        ];
+        return this.sizeList;
+    }
+
+    _renderStuffingList() {
+        this.stuffingList = [
             {name: 'Cheese', price: 10, calories: 20},
             {name: 'Salad', price: 20, calories: 5},
             {name: 'Fries', price: 15, calories: 10}
         ];
-        result = this.stuffing.find(el => el.name === stuffing);
-        return result;
+        return this.stuffingList;
     }
 
-    addTopping(topping) {
-
-    }
-    removeTopping(topping) {
-
-    }
-    getToppings(toppings) {
-        let result = [];
-        this.toppings = [
+    _renderToppingList() {
+        this.toppingList = [
             {name: 'Seasoning', price: 15, calories: 0},
             {name: 'Mayonnaise', price: 20, calories: 5}
         ];
-        result = this.toppings.filter(el => toppings.find(element => element === el.name));
-        return result;
+        return this.toppingList;
     }
 
     getSize(size) {
-        let result;
-        this.size = [
-            {name: 'Small', price: 50, calories: 20},
-            {name: 'Big', price: 100, calories: 40}
-        ];
-        result = this.size.find(el => el.name === size);
+        let result = this.sizeList.find(el => el.name === size);
         return result;
 
     }
+
+    getStuffing(stuffing) {
+        let result = this.stuffingList.find(el => el.name === stuffing);
+        return result;
+    }
+
+    addTopping(toppingName) {
+        let result = this.getToppings([toppingName]);
+        result.find(el => this.toppings.push(el.name === toppingName));
+        return this.toppingList;
+    }
+
+    removeTopping(topping) {
+        this.toppings = this.toppings.filter(el => el.name != topping);
+        return this.toppings;
+    }
+
+    getToppings(toppings) {
+        let result = this.toppingList.filter(el => toppings.find(element => element === el.name));
+        return result;
+    }
+
     calculatePrice() {
         let totalPrice;
         totalPrice = this.size.price + this.stuffing.price;
@@ -50,6 +68,7 @@ class Hamburger {
 
 
     }
+
     calculateCalories() {
         let totalCalories;
         totalCalories = this.size.calories + this.stuffing.calories;
@@ -59,7 +78,7 @@ class Hamburger {
 }
 
 let smallHamburger = new Hamburger('Small', 'Salad', ['Seasoning', 'Mayonnaise']);
-console.log(smallHamburger.calculatePrice(), smallHamburger.calculateCalories());
+console.log(smallHamburger.calculatePrice(), smallHamburger.calculateCalories(), smallHamburger.removeTopping('Ketchup'));
 
 let bigHamburger = new Hamburger('Big', 'Cheese', ['Seasoning']);
-console.log(bigHamburger.calculatePrice(), bigHamburger.calculateCalories());
+console.log(bigHamburger.calculatePrice(), bigHamburger.calculateCalories(), bigHamburger.addTopping('Mayonnaise'));
