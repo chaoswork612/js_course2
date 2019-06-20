fe171b32339fddbaa10c6154df04bfa2a095d9f6
chaoswork612 <konstantin-morozov80@yandex.ru>
2019-06-21 00:14:48 +0300

    Lesson 6 after review

--- project_comp_templates/js/SearchComponent.js
+++ project_comp_templates/js/SearchComponent.js
@@ -1,7 +1,8 @@
 Vue.component('search-form', {
-    props: ['search-form', 'userSearch'],
-    template: `<form action="#" method="post" class="search-form" @submit.prevent="$parent.$emit(filter)">
-                    <input type="text" class="search-field" :value="userSearch">
+    props: ['userSearch'],
+    template: `<form action="#" method="post" class="search-form" @submit.prevent="$emit('filter')">
+                    <input type="text" class="search-field" :value="userSearch"
+                    @input="$emit('input', $event.target.value)">
                     <button class="btn-search" type="submit">
                     <i class="fas fa-search"></i>
                     </button>
